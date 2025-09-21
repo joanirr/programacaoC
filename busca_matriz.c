ac9705fe2c766c5606a4e5a685109cc2d85e25d1
joanirr <joanirklod@gmail.com>
2025-09-21 21:05:53 +0000

    new_commit

--- busca_matriz.c
+++ busca_matriz.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
+#define LINHAS 3
+#define COLUNAS 3
  
 int main() {
-    int matriz[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int matriz[LINHAS][COLUNAS];
     int target = 5;
     int found = 0;
+    int soma = 1;
+
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            matriz[i][j] = soma;
+            soma++;
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
  
     // Busca condicional do elemento alvo
-    for (int i = 0; i < 3; i++) {      // Loop externo para as linhas
-        for (int j = 0; j < 3; j++) {  // Loop interno para as colunas
+    for (int i = 0; i < LINHAS; i++) {      // Loop externo para as linhas
+        for (int j = 0; j < COLUNAS; j++) {  // Loop interno para as colunas
             if (matriz[i][j] == target) {
                 printf("Elemento %d encontrado na posição (%d, %d)\n", target, i, j);
                 found = 1;
